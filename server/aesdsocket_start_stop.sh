a9bf4fb3a474f7e14d46612b3a23570bc57b71dd
pvskpraveen <pvskpraveen@yahoo.com>
2023-06-26 19:40:10 -0600

    assignment5p1

---
+++ server/aesdsocket_start_stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case "$1" in
+	start)
+		echo "starting simple server"
+		start-stop-daemon -S -n aesdsocket-start-stop -a /usr/bin/aesdsocket
+		;;
+	stop)
+		echo "stopping simple server"
+		start-stop-daemon -K -n aesdsocket-start-stop
+		;;
+	*)
+		echo "Usage: $0 {start | stop}"
+	exit 1
+esac
+
+exit 0
--- server/val_grinned.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=/tmp/valgrind-out.txt ./aesdsocket
